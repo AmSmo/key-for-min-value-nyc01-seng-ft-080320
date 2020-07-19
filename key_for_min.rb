ac85bf92feb3e3e8918097dfc17e243d838d8de8
Adam Smolenski <adam.smolenski@gmail.com>
2020-07-19 18:21:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,13 @@
 
 def key_for_min_value(name_hash)
   current = 1000
+  winner = :bob
   name_hash.each do |key, value|
     if key[value] < current
       current = key[value]
+      winner = key
     end
   end
-  current 
+  winner 
   
 end
